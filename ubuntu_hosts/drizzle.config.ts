0b3ad8f06ee6a3b6946bd27fc837b57d1aaeca61
Siraj Abdulkadir <imsirajabdulkadir@gmail.com>
2026-06-24 23:15:27 +0300

    chore:generated migration files,changed the frontend port to 5000
    Co-authored-by: Abel Mathios <7gebruabel@gmail.com>

--- ubuntu_hosts/drizzle.config.ts
+++ ubuntu_hosts/drizzle.config.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
-  out: './drizzle',
+  out: './server/db/migrations',
   schema: [
     './server/db/schema.ts',
     './server/modules/**/*.schemas.ts'
@@ -13,4 +13,3 @@ export default defineConfig({
   },
   casing:'snake_case'
 });
- 
