73439e8e40cd9ee3a3813fb39d99729ce298f68e
Siraj Abdulkadir <imsirajabdulkadir@gmail.com>
2026-06-27 03:21:09 +0300

    fix:api url and enviroment variables fix

--- ubuntu_hosts/client/src/lib/auth-client.ts
+++ ubuntu_hosts/client/src/lib/auth-client.ts
@@ -1,4 +1,7 @@
 import { createAuthClient } from "better-auth/react";
+
+const apiUrl = import.meta.env.VITE_API_URL;
+
 export const authClient = createAuthClient({
-  baseURL: "https://ubuntu-hosts-5zts.onrender.com", 
+  baseURL:apiUrl, 
 });
